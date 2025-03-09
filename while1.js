d5548e9059d97fbd60d3cc19cdf153b062add6c3
hasanhameed <hasanhameed858@gmail.com>
2025-03-09 15:22:46 +0530

    Series except 5 multiples

--- while1.js
+++ while1.js
@@ -1,14 +1,17 @@
 /* Print the following series 
        1 4 9 16 25 36 to n. Note print all the numbers in a seperate line*/
 let number = 1;
-let count = 2;
+let n = 49;
+let pattern_number = 1;
 while (n > 0) {
-  console.log(number);
-  number = number + 1;
-  number = number + count;
-  count = count * 2;
-  if (number > n) {
-    break;
+  if (number == 1) {
+    console.log(1);
   }
+  number = number + 2;
+  pattern_number = pattern_number + number;
+
+  if (pattern_number < n) {
+    console.log(pattern_number);
+  }
+  n--;
 }
-console.log("hello");
